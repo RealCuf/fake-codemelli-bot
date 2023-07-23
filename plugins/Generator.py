bed8e06ff97396d5788aaa1bbfc084b12f95ddd9
Kasra <kasrixsh@gmail.com>
2023-07-24 01:33:42 +0330

    added command

--- plugins/Generator.py
+++ plugins/Generator.py
@@ -1,6 +1,6 @@
 import random
 
-# Code Melli Generator
+#code generator
 
 def default_generator():
     default = ""
@@ -13,18 +13,19 @@ def default_generator():
     default += str(control_number)
     return default
 
-# Code Melli Calculate
+#code calculate
 
 def calculate_control_number(melli_code):
     melli_code = list(map(int, melli_code))
     control_sum = sum(melli_code[i] * (10 - i) for i in range(9))
     control_number = control_sum % 11
+
     if control_number < 2:
         return control_number
     else:
         return 11 - control_number
 
-# Round Code Melli Generator
+#round code generator
 
 def round_generator():
     round = []
@@ -36,11 +37,12 @@ def round_generator():
         round.append(j)
         code_sum += j * i
 
-    s = code_sum % 11
-    if s < 2:
-        round.append(s)
+    cs = code_sum % 11
+
+    if cs < 2:
+        round.append(cs)
     else:
-        round.append(11 - s)
+        round.append(11 - cs)
 
     if all(x == round[0] for x in round[1:]):
         return round_generator()
