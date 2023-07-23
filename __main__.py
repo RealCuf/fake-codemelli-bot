6a2127e871bbb14dd3d141091ae0b4ef4815e30e
Kasra <kasrixsh@gmail.com>
2023-07-24 02:59:10 +0330

    edit main

--- __main__.py
+++ __main__.py
@@ -3,11 +3,11 @@ from pyrogram import Client, idle
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
 
-BOT_TOKEN = os.environ.get('BOT_TOKEN','6109073550:AAHky1JxYGTXKLfSI6KYJGYE5nOzk0g35AE') #BOT_TOKEN
+BOT_TOKEN = os.environ.get('BOT_TOKEN','#') #BOT_TOKEN
 
-APP_ID = int(os.environ.get('API_ID','14898747')) #API_ID
+APP_ID = int(os.environ.get('API_ID','#')) #API_ID
 
-API_HASH = os.environ.get('API_HASH','396a12867419b192e537bcd5bed1f9b4') #API_HASH
+API_HASH = os.environ.get('API_HASH','#') #API_HASH
 
 
 plugins = dict(
